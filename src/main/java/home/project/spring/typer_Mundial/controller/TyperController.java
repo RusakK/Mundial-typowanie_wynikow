3cb44cc5fe3740972dc0378acdbf6e486c3d3d88
krissuNew <krzysztofrusak91@gmail.com>
2018-07-05 00:11:37 +0200

    zmiana wyglądu stron

--- src/main/java/home/project/spring/typer_Mundial/controller/TyperController.java
+++ src/main/java/home/project/spring/typer_Mundial/controller/TyperController.java
@@ -4,12 +4,14 @@ import home.project.spring.typer_Mundial.model.Typer;
 import home.project.spring.typer_Mundial.service.TyperDataService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/mundial")
 public class TyperController {
 
     private final TyperDataService typerDataService;
@@ -18,18 +20,28 @@ public class TyperController {
         this.typerDataService = typerDataService;
     }
 
-    @RequestMapping(value = "/show", method = RequestMethod.GET)
-    public String allTypers(Model model) {
+
+    @RequestMapping
+    public String showRanking(Model model) {
+        System.out.println("Pokaz ranking");
         List<Typer> typers = typerDataService.loadAllTypers();
         model.addAttribute("allTypers", typers);
-        model.addAttribute("typer", new Typer());
-        return "showGames";
+        return "mundial";
     }
 
-    @RequestMapping(value = "/show", method = RequestMethod.POST)
-    public String saveTyper(Typer typer) {
-        typerDataService.addTyper(typer);
-        return "redirect:/show";
+    @GetMapping("/newTyper")
+    public String prepareNewTyper(Model model) {
+        System.out.println("Przygotowanie nowego typera");
+        model.addAttribute("newTyper", new Typer());
+        return "newTyper";
+    }
+
+    @GetMapping("/{typerId}/typowanie")
+    public String typeMatches(
+            @PathVariable("typerId") Integer typerId,
+            Model model) {
+        model.addAttribute("typerId", typerId);
+        return "typowanie";
     }
 
 }
